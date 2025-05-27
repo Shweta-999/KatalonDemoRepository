c0ea39b5b604899859d69d71e6b5c88da11d2b90
289634 <289634@192.168.1.4>
2025-05-27 16:23:40 +0530

    4th commit

--- Scripts/ListenersDemo/Script1747810810289.groovy
+++ Scripts/ListenersDemo/Script1747810810289.groovy
@@ -17,4 +17,5 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-println ' i am inside the test case'
+println (' \n i am inside the test case \n')
+
